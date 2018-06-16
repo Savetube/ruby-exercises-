c5dd0170f6b662a6c3f672a4bb80df6dd23e5b87
Savetube <Saveutube@gmail.com>
2018-06-17 01:01:33 +0800

    new exercises added

--- ex6.rb
+++ ex6.rb
@@ -1,21 +1,49 @@
+# initialize the variable to a numerical value below
 types_of_people = 10
+
+# passed a value of string within a string to a variable
 x = "There are #{types_of_people} types of people."
+
+# passed a string value to a variable of the same characters.
 binary = "binary"
+
+# passed a string value to another variable
 do_not = "don't"
+
+# passed a value of string within a string to a variable
 y = "Those who know #{binary} and those who #{do_not}."
 
+# print to screen the value of x
 puts x
+
+# print to screen the value of y
 puts y
 
+# print a statement with the value x inserted in it
 puts "I said: #{x}"
+
+# print a statement with the value y inserted in it
 puts "I also said: #{y}"
 
+# declare and initialize another variable
 hilarious = false
+
+# initialize and pass a string variable inserted to a string
 joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
 
+
+# print to screen the other variable
 puts joke_evaluation
 
+
+# intialize another string variable
 w = "This is the left side of..."
+
+# initialize one more string variable
 e = "a string with a right side."
 
+# print to screen the values of two string variable together.
 puts w + e
+
+# check whether single quote can replace double quotes
+puts 'This sentence will confirm single quote can replace double quotes.'
